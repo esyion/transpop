bc8290f130982c582588db0ba051c919baf2e79d
esyion <qingbomy@gmail.com>
2026-09-09 14:34:00 +0800

    1

--- src/components/ui/sonner.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { Toaster as SonnerToaster } from "sonner";
-
-const Toaster = (props: React.ComponentProps<typeof SonnerToaster>) => {
-  return <SonnerToaster richColors position="top-center" {...props} />;
-};
-
-export { Toaster };
--- src/lib/runtime.ts
+++
@@ -1,2 +0,0 @@
-export const hasTauriRuntime = () =>
-  typeof window !== "undefined" && "__TAURI_INTERNALS__" in window;
